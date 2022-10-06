31699b51e27bad7759329199c2340ff6cd499dac
Egor Sporysh <egorsporush13791995@gmail.com>
2022-10-06 15:37:37 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function() {
+  $('.header__burger').click(function() {
+      $('.header__burger').toggleClass('active');
+      $('.header__nav').toggleClass('active');
+  });
+
+});
+
+
